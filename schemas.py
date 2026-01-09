9fe2096019ef6c5ba20c9a3b6e8a6d1995cc0389
peddireddytharunkumarreddy <tharunkumarreddy443@gmail.com>
2026-01-09 11:34:59 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, ConfigDict
+from typing import Optional
 
 class StudentBase(BaseModel):
     name: str
@@ -9,8 +10,11 @@ class StudentBase(BaseModel):
 class StudentCreate(StudentBase):
     pass
 
-class StudentUpdate(StudentBase):
-    pass
+class StudentUpdate(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    email: Optional[str] = None
+    course: Optional[str] = None
 
 class StudentResponse(StudentBase):
     id: int
